d7f7e2a3ff1e3564202463986fd18d95a4e6a0c8
Dejan Toteff <deyan8284@gmail.com>
2025-10-05 15:28:53 +0300

    chore@small

--- src/main.tsx
+++ src/main.tsx
@@ -25,33 +25,32 @@ export function MainApp() {
 		}
 	}
   return (
-    <div className='flex flex-col items-center justify-center h-screen'>
+    <div className='flex flex-col items-center justify-center h-screen gap-8'>
+      <div className='flex flex-col items-center justify-center'>
+        <input type='file' onChange={handleFileChange} />
+      </div>
       
-			<div className='flex flex-col items-center justify-center h-screen'>
-				<input type='file' onChange={handleFileChange} />
-			</div>
-			
-			<div className='flex flex-col items-center justify-center h-screen'>
-				<Table>
-        <TableCaption>A list of your recent invoices.</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead className='w-[100px]'>Invoice</TableHead>
-            <TableHead>Status</TableHead>
-            <TableHead>Method</TableHead>
-            <TableHead className='text-right'>Amount</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          <TableRow>
-            <TableCell className='font-medium'>INV001</TableCell>
-            <TableCell>Paid</TableCell>
-            <TableCell>Credit Card</TableCell>
-            <TableCell className='text-right'>$250.00</TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-    </div>
+      <div className='flex flex-col items-center justify-center'>
+        <Table>
+          <TableCaption>A list of your recent invoices.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className='w-[100px]'>Invoice</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Method</TableHead>
+              <TableHead className='text-right'>Amount</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell className='font-medium'>INV001</TableCell>
+              <TableCell>Paid</TableCell>
+              <TableCell>Credit Card</TableCell>
+              <TableCell className='text-right'>$250.00</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
     </div>
   )
 }
