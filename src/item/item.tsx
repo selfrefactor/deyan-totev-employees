ad2f3b8f7cf6c34b16054d78bf0417564fec3c16
Dejan Toteff <deyan8284@gmail.com>
2025-10-26 21:26:51 +0200

    chore@small

--- src/item/item.tsx
+++ src/item/item.tsx
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
@@ -14,8 +15,23 @@ export function Item() {
   if (!data) return null
 
   return (
-    <div className='flex flex-col items-center justify-center h-screen'>
-      <h1 className='text-2xl font-bold'>{data.title}</h1>
+    <div className='flex flex-col items-start ml-12 mt-12 h-screen'>
+      <h1 className='text-2xl font-bold'>Title: {data.title}</h1>
+      <p>Description: {data.description}</p>
+      <p>Category: {data.category}</p>
+      <p>Estimated Value: {data.estimatedValue}</p>
+      <p>
+        <div className='w-full'>
+          <img
+            alt={data.title}
+            className='w-full h-full object-cover'
+            src={data.imageUrl}
+          />
+        </div>
+      </p>
+      <p>Auction House: {data.auctionHouse}</p>
+      <p>End Date: {dayjs(data.endDate).format('DD/MM/YYYY HH:mm')}</p>
+      <p>Status: {data.status}</p>
     </div>
   )
 }
